888bbe3ba5b39b9746cfabb178007a4ba6cc025c
Kris Brown <kris@topos.institute>
2023-03-21 17:44:28 -0700

    working patient 0

--- src/Complexvid.jl
+++ src/Complexvid.jl
@@ -10,13 +10,9 @@ using AlgebraicRewriting
 #############################################
 # schema
 #############################################
-@present SchHalfEdgeCO(FreeSchema) begin
-  V::Ob
-  H::Ob
+@present SchHalfEdgeCO <: SchSymmetricGraph begin
   (S, Ia, Im, Is, Ic, R, D)::Ob
 
-  vertex::Hom(H,V)
-  inv::Hom(H,H)
   s::Hom(S,V)
   ia::Hom(Ia,V)
   im::Hom(Im,V)
@@ -25,10 +21,8 @@ using AlgebraicRewriting
   r::Hom(R,V)
   d::Hom(D,V)
 
-  compose(inv, inv) == id(H)
-
   (Name,Num)::AttrType
-  layer::Attr(H,Name)
+  layer::Attr(E,Name)
   group::Attr(V,Name)
   daysIa::Attr(Ia, Num)
   daysIm::Attr(Im, Num)
@@ -38,10 +32,11 @@ using AlgebraicRewriting
   symptomsIm::Attr(Im, Num)
   symptomsIc::Attr(Ic, Num)
   symptomsIs::Attr(Is, Num)
+
 end
 
 
-@acset_type AbstractHalfEdgeCO(SchHalfEdgeCO, index=[:vertex])
+@acset_type AbstractHalfEdgeCO(SchHalfEdgeCO) <: AbstractGraph
 const HalfEdgeCO = AbstractHalfEdgeCO{Symbol,Int}
 
 h2 = @acset HalfEdgeCO begin
@@ -50,8 +45,6 @@ h2 = @acset HalfEdgeCO begin
   layer = [:L1]
   group = [:G1]
 end
-
-print(h2)
 
 
 
@@ -67,7 +60,9 @@ print(h2)
 InfectMildL = @acset HalfEdgeCO begin V=1; S=1; Name=1; s=1; group=[AttrVar(1)] end
 InfectMildI = @acset HalfEdgeCO begin V=1; Name=1; group=[AttrVar(1)] end
 InfectMildR = @acset HalfEdgeCO begin V=1; Im=1; Num=1; Name=1; im=1; group=[AttrVar(1)]; daysIm=[0]; symptomsIm=[AttrVar(1)];  end
-InfectMild = Rule(homomorphism(InfectMildI, InfectMildL), homomorphism(InfectMildI, InfectMildR); expr=(Num=[vs->rand(Int)],))
+InfectMild = Rule(homomorphism(InfectMildI, InfectMildL), 
+                  homomorphism(InfectMildI, InfectMildR); 
+                  expr=(Num=[vs->rand(Int)],))
 
 #############################################
 # Schedule
@@ -76,7 +71,7 @@ InfectMild = Rule(homomorphism(InfectMildI, InfectMildL), homomorphism(InfectMil
 # Disease progression (showing symptoms, recovery, diagnosis, isolation)
 # 
 
-average_degree(g::HalfEdgeCO) = sum(degree.(vertices(g)))/nparts(g,:V)
+average_degree(g::HalfEdgeCO) = sum([degree(g,v) for v in vertices(g)])/nparts(g,:V)
 close_to_average(g::HalfEdgeCO, v::Int) = abs(degree(g,v) - average_degree(g)) <= 1
 # Convert a homomorphism (V -> world state) into a pair, worldstate + vertex id
 close_to_average(f::ACSetTransformation) = close_to_average(codom(f), f[:V](1)) 
--- test/test.jl
+++ test/test.jl
@@ -15,18 +15,28 @@ X = @acset HalfEdgeCO begin V=2; S=2; s=[1,2]; group=[:old,:young] end
 
 I = HalfEdgeCO()
 v = @acset HalfEdgeCO begin V=1; Name=1; group=[AttrVar(1)] end
-r = RuleApp("infectMild", InfectMild, id(v)) # schedule 
+r = RuleApp("infectMild", InfectMild, id(v)) # agent map given by A -> I
 view_sched(singleton(r))
-i = if_cond("closeToAverage", close_to_average, v) # schedule 
+i = if_cond("closeToAverage", close_to_average, v; argtype=:agent) # schedule 
 view_sched(singleton(i))
 
 
-q = Query("dot", I, v)
+q = Query("dot", v, I)
 view_sched(singleton(q))
 
-mk_sched((trace_arg=:V,init=:I), 1, (V=v,I=I, avg=i, quer=q, rw=r), begin 
-  q1,q2,q3 = quer(init,trace_arg)
+sched = mk_sched((init=:I, trace_arg=:V), 1, 
+                 (V=v,I=I, avg=i, quer=q, rw=r, w=Weaken("",create(v))), quote 
+  q1,q2,q3 = quer(init,trace_arg,)
   avg1, avg2 = avg(q2)
   inf1, inf2 = rw(avg1)
-  return ([inf2, avg2],[q1,q3,inf1])
+  return ([q1,q3,w(inf1)], [inf2, avg2])
 end)
+
+typecheck(sched)
+
+view_sched(sched)
+
+traj = apply_schedule(sched, X; verbose=true)
+
+
+
