12a52945a08fdc95f47d80edcd391d59c1cc3e5c
gkreder <gkreder@gmail.com>
2023-03-23 17:17:30 -0700

    mild infections routine draft

--- test/test.jl
+++ test/test.jl
@@ -7,6 +7,19 @@ using AlgebraicRewriting
 
 
 
+########################################
+# Agent Types
+########################################
+Av = @acset HalfEdgeCO begin V=1; Name=1; group=[AttrVar(1)]; end
+AIm = @acset HalfEdgeCO begin 
+  V=1; Im=1; im=1; Name=1; Num=2; group=[AttrVar(1)]; daysIm=[AttrVar(1)]; symptomsIm=[AttrVar(2)] 
+end
+AIm0 = @acset HalfEdgeCO begin V=1; Im=1; Name=1; Num=1; im=1; group=[AttrVar(1)]; daysIm=[AttrVar(1)]; symptomsIm=[0] end
+I = HalfEdgeCO()
+
+N = Dict([I=>"", Av=>"•", AIm=>"Im", AIm0=>"Im0"]) # acsets to strings dict
+
+
 
 # Tests 
 ########
@@ -17,20 +30,20 @@ using AlgebraicRewriting
 X = @acset HalfEdgeCO begin V=2; S=2; s=[1,2]; group=[:old,:young] end
 # rewrite(InfectMild, X)
 
-I = HalfEdgeCO()
-v = @acset HalfEdgeCO begin V=1; Name=1; group=[AttrVar(1)] end
-r = RuleApp(:infectMild, InfectMild, id(v)) # agent map given by A -> I
-N = Dict([I=>"", v=>"•"]) # acsets to strings dict
-view_sched(singleton(r); names=N)
-i = if_cond(:closeToAverage, close_to_average, v; argtype=:agent) # schedule 
+# I = HalfEdgeCO()
+# v = @acset HalfEdgeCO begin V=1; Name=1; group=[AttrVar(1)] end
+initial_infect_r = RuleApp(:infectMild, InfectMild, id(Av)) # agent map given by A -> I
+# N = Dict([I=>"", Av=>"•"]) # acsets to strings dict
+view_sched(singleton(initial_infect_r); names=N)
+i = if_cond(:closeToAverage, close_to_average, Av; argtype=:agent) # schedule 
 view_sched(singleton(i); names=N)
 
 
-q = Query(:dot, v, I)
+q = Query(:dot, Av, I)
 view_sched(singleton(q), names=N)
 
 sched = mk_sched((trace_arg=:V,), (init=:I,), 
-                 (V=v,I=I, avg=i, quer=q, rw=r, w=Weaken(create(v))), quote 
+                 (V=Av,I=I, avg=i, quer=q, rw=initial_infect_r, w=Weaken(create(Av))), quote 
   q1,q2,q3 = quer(init,trace_arg,)
   avg1, avg2 = avg(q2)
   inf1, inf2 = rw(avg1)
@@ -68,11 +81,11 @@ Increment = Rule(homomorphism(IncrementI, IncrementL),
 
 
 X = @acset HalfEdgeCO begin V=2; S=1; s=[1]; Im=1; im=2; daysIm=[1]; symptomsIm=[2]; group=[:old,:young]; end
-v = @acset HalfEdgeCO begin V=1; Im=1; Name=1; Num=2; im=1; group=[AttrVar(1)]; daysIm=[AttrVar(1)]; symptomsIm=[AttrVar(2)] end
-r = RuleApp(:increment, Increment, id(v)) # agent map given by A -> I
-view_sched(singleton(r))
+# v = @acset HalfEdgeCO begin V=1; Im=1; Name=1; Num=2; im=1; group=[AttrVar(1)]; daysIm=[AttrVar(1)]; symptomsIm=[AttrVar(2)] end
+increment_r = RuleApp(:increment, Increment, id(AIm)) # agent map given by A -> I
+view_sched(singleton(increment_r), names = N)
 
-traj = apply_schedule(singleton(r), homomorphism(v, X); verbose=true)
+traj = apply_schedule(singleton(increment_r), homomorphism(AIm, X); verbose=true)
 traj.initial.codom
 traj.steps[1].world.codom
 
@@ -107,10 +120,13 @@ symptoms = Rule(homomorphism(symptomsI, symptomsL; monic=true),
      id(symptomsI); 
      ac=[AppCond(homomorphism(symptomsL, symptomsN), false)])
 
-A = @acset HalfEdgeCO begin V=1; Im=1; Name=1; Num=1; im=1; group=[AttrVar(1)]; daysIm=[AttrVar(1)]; symptomsIm=[0] end
+# symptoms_r = RuleApp(:Symptoms, symptoms, homomorphism(AIm0, symptomsI))
+symptoms_r = tryrule(RuleApp(:Symptoms, symptoms, homomorphism(AIm, symptomsI)))
 
-symptoms_r = RuleApp(:Symptoms, symptoms, homomorphism(A, symptomsI))
-view_sched(singleton(symptoms_r))
+# homomorphisms(AIm0, symptomsI)
+# homomorphisms(AIm, symptomsI)
+
+view_sched(symptoms_r, names = N)
 X = @acset HalfEdgeCO begin 
   V=3; Im=2; im=[1,2]; daysIm=[2, 4]; symptomsIm=[2, 0]; 
   S=1; s=3;
@@ -120,18 +136,12 @@ X = @acset HalfEdgeCO begin
   group=[:Old,:Old,:Young];
   # layer=[]
 end
-traj = apply_schedule(singleton(symptoms_r), homomorphism(A, X); verbose=true)
+traj = apply_schedule(symptoms_r, homomorphisms(AIm, X)[2]; verbose=true)
 traj.initial.codom
 traj.steps[1].world.codom
 
-
-
-# get_matches(symptoms, X, verbose = true)
-# homomorphisms(symptomsL, X)
-
-# loop_rule
-
-
+# get_matches(symptoms, X)
+# homomorphisms(AIm, X)[2]
                 
 ########################################
 # Recovery
@@ -147,9 +157,9 @@ RecoverR = @acset HalfEdgeCO begin V=1; R=1; r=1; Name=1; group=[AttrVar(1)];  e
 RecoverMild = Rule(homomorphism(RecoverI, RecoverL), homomorphism(RecoverI, RecoverR))
 
 # A = @acset HalfEdgeCO begin V=1; Im=1; Name=1; Num=2; im=1; group=[AttrVar(1)]; daysIm=[AttrVar(1)]; symptomsIm=[AttrVar(2)] end
-A = @acset HalfEdgeCO begin V=1; Name=1; group=[AttrVar(1)]; end
+# A = @acset HalfEdgeCO begin V=1; Name=1; group=[AttrVar(1)]; end
 
-recoverMildApp = tryrule(RuleApp(:RecoverMild, RecoverMild, homomorphism(A, RecoverI)))
+recoverMildApp = tryrule(RuleApp(:RecoverMild, RecoverMild, homomorphism(Av, RecoverI)))
 # r = RuleApp(:RecoverMild, RecoverMild, homomorphism(A, RecoverL), ) # explicitly giving transformations A -> L (first) and A -> R?
 # homomorphisms(A, RecoverL)
 # homomorphisms(A, RecoverR)
@@ -158,7 +168,7 @@ recoverMildApp = tryrule(RuleApp(:RecoverMild, RecoverMild, homomorphism(A, Reco
 # id(A)
 # N = Dict([I=>"", v=>"•"]) # acsets to strings dict
 
-view_sched(recoverMildApp)
+view_sched(recoverMildApp, names = N)
 X = @acset HalfEdgeCO begin 
   V=2; Im=1; im=[1]; daysIm=[18]; symptomsIm=[0]; 
   S=1; s=2;
@@ -167,18 +177,13 @@ X = @acset HalfEdgeCO begin
   layer=[:Home,:Home];
   group=[:Old,:Young];
 end
-traj = apply_schedule(recoverMildApp, homomorphism(A, X); verbose=true)
+traj = apply_schedule(recoverMildApp, homomorphism(Av, X); verbose=true)
 traj.initial.codom
 traj.steps[1].world.codom
 
 ########################################
 # Flip Coin
 ########################################
-
-
-A = @acset HalfEdgeCO begin 
-  V=1; Im=1; im=1; Name=1; Num=2; group=[AttrVar(1)]; daysIm=[AttrVar(1)]; symptomsIm=[AttrVar(2)] 
-end
 
 X = @acset HalfEdgeCO begin 
   V=2; Im=1; im=[1]; daysIm=[21]; symptomsIm=[-10]; 
@@ -198,20 +203,14 @@ function rf(f::ACSetTransformation)
   return([p, 1 - p])
 end
 
-# I = HalfEdgeCO()
-v = @acset HalfEdgeCO begin V=1; Name=1; group=[AttrVar(1)] end
-N = Dict([HalfEdgeCO()=>"", v=>"•", A=>"Im"])
-recoveryFlip = Conditional(rf, 2, A; name=:recoveryFlip, argtype=:agent)
+recoveryFlip = Conditional(rf, 2, AIm; name=:recoveryFlip, argtype=:agent)
 view_sched(singleton(recoveryFlip), names=N)
 
 
-
-
-
-average_degree(g::HalfEdgeCO) = sum([degree(g,v) for v in vertices(g)])/nparts(g,:V)
-close_to_average(g::HalfEdgeCO, v::Int) = abs(degree(g,v) - average_degree(g)) <= 1
-# Convert a homomorphism (V -> world state) into a pair, worldstate + vertex id
-close_to_average(f::ACSetTransformation) = close_to_average(codom(f), f[:V](1))
+# average_degree(g::HalfEdgeCO) = sum([degree(g,v) for v in vertices(g)])/nparts(g,:V)
+# close_to_average(g::HalfEdgeCO, v::Int) = abs(degree(g,v) - average_degree(g)) <= 1
+# # Convert a homomorphism (V -> world state) into a pair, worldstate + vertex id
+# close_to_average(f::ACSetTransformation) = close_to_average(codom(f), f[:V](1))
 
 
 function twenty_days_sick(f::ACSetTransformation)
@@ -221,24 +220,45 @@ function twenty_days_sick(f::ACSetTransformation)
   return(d >= 20)
 end
 
-i20 = if_cond(:twentyDaysSick, twenty_days_sick, A; argtype=:agent) # schedule 
+i20 = if_cond(:twentyDaysSick, twenty_days_sick, AIm; argtype=:agent) # schedule 
 view_sched(singleton(i20), names = N)
 
-AGeneric = @acset HalfEdgeCO begin V=1; Name=1; group=[AttrVar(1)]; end
-AIm = @acset HalfEdgeCO begin 
-  V=1; Im=1; im=1; Name=1; Num=2; group=[AttrVar(1)]; daysIm=[AttrVar(1)]; symptomsIm=[AttrVar(2)] 
+function is_symptom_day(f::ACSetTransformation)
+  X = f.codom
+  i = f[:Im](1)
+  d = X[i,:daysIm]
+  return(d == 0)
 end
 
-q_im = Query(:InfectedPeople, AIm, I, AGeneric) 
+checkI0 = if_cond(:symptomDay, is_symptom_day, AIm; argtype=:agent)
+
+q_im = Query(:InfectedPeople, AIm, I, Av) 
+
 sched_rec = mk_sched((trace_arg=:G,), (init=:I,), 
-                 (A=AIm, I=I, G=AGeneric, recFlip=recoveryFlip, q=q_im,
+                 (A=AIm, I=I, G=Av, A0=AIm0, recFlip=recoveryFlip, q=q_im,
                   recoverMildApp=recoverMildApp, itw=i20, f=Fail(I),
-                  w=Weaken(homomorphism(AGeneric,AIm))), quote 
+                  cio=checkI0, sr=symptoms_r, w=Weaken(homomorphism(Av,AIm))), quote 
   final_output, loop_start, ignore = q(init, trace_arg)
   f(ignore)
-  afterTwenty, beforeTwenty = itw(loop_start) 
+  symptomDay, notSymptomDay = cio(loop_start)
+  quarantined = sr(symptomDay)
+  afterTwenty, beforeTwenty = itw([quarantined, notSymptomDay]) 
   wonFlip, lostFlip = recFlip(beforeTwenty) 
   recovered1 = recoverMildApp(w([afterTwenty, wonFlip]))
   return ([w(lostFlip), recovered1], final_output)
 end)
-view_sched(sched;names=N)
+
+view_sched(singleton(q_im), names = N)
+view_sched(singleton(checkI0), names = N)
+view_sched(symptoms_r, names = N)
+view_sched(singleton(i20), names = N)
+view_sched(singleton(recoveryFlip), names = N)
+view_sched(sched_rec;names=N)
+
+
+
+
+# homomorphism(AIm,AIm0)
+
+AIm0
+checkI0
