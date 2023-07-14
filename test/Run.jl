279ce87685eb1c818b9e0b4816d51cb292c7d5a2
Kris Brown <kris@topos.institute>
2023-07-14 11:35:16 -0700

    In place rewriting example

--- test/Run.jl
+++ test/Run.jl
@@ -1,15 +1,26 @@
 module TestRun 
 using Complexvid
-using AlgebraicRewriting, Catlab
+using AlgebraicRewriting, Catlab, ACSets
 using Luxor
 using Random
 
 Random.seed!(1)
 
-X = initialize()
-
-sched = overall(1); # just one day
+sched = overall(100); # just one day
 view_sched(sched;names=Name)
+
+
+
+ssched = sparsify(sched);
+sX = sparsify(initialize(100))
+Random.seed!(1)
+@time interpret!(ssched, sX)
+
+sX
+
+im = InfectMild() |> sparsify
+ms = homomorphisms(codom(left(im)), sX)
+can_match.(Ref(im), ms)
 
 traj, = apply_schedule(sched, X; steps=100);
 traj_res(traj).steps[end].world |> codom
